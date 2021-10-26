260863af18f0618744ce4ef26497ca4d31627392
EloiGry <eloi.grychta@gmail.com>
2021-10-26 16:40:22 +0200

    modif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from "react"
+import 'antd/dist/antd.css'
 import './App.css';
+import Counter from './components/Counter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      count: 0,
+      count2: 0
+    }
+  }
+
+  handlePlusClick = () => {
+    if (this.state.count <= this.state.count2) {
+      this.setState({ count: this.state.count + 1 })
+    }  
+    if (this.state.count === this.state.count2) {
+      this.setState({ count2: this.state.count2 + 1 })
+    }
+  } 
+
+  handleMoinsClick = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 })
+    }
+  }
+
+  handlePlusClick2 = () => {
+    this.setState({ count2: this.state.count2 + 1 })
+      
+  } 
+
+
+  handleMoinsClick2 = () => {
+    if (this.state.count2 > 0) {
+      this.setState({ count2: this.state.count2 - 1 })
+    }
+    if (this.state.count === this.state.count2 && this.state.count !== 0) {
+      this.setState({ count: this.state.count - 1 })
+    }
+  }
+    render() {
+      return (
+        <> 
+          <h1>
+            Counter
+          </h1>
+          <Counter count={this.state.count} increment={this.handlePlusClick} substract={this.handleMoinsClick}/>
+          <Counter count={this.state.count2} increment={this.handlePlusClick2} substract={this.handleMoinsClick2}/>
+        </>
+
+          
+      );
+    }
+
 }
+
 
 export default App;
